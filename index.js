d750ce7dfb001de05618c510ecd082838d86617c
vinayak-naik <vinayaknaik2021@gmail.com>
2023-12-29 11:56:28 +0530

    update11

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-// app.js
-const express = require('express');
+// Import packages
+const express = require("express");
+
+// Middlewares
 const app = express();
 app.use(express.json());
-const dotenv = require('dotenv');
 
-dotenv.config();
-const port = process.env.PORT || 3000;
-
+// Routes
 // Define a test route
 app.get('/test', (req, res) => {
-  res.send('This is a test route!');
-});
-app.get('/', (req, res) => {
-  res.send('Route not found!  --vinayak');
-});
+    res.send('This is a test route!');
+  });
+  app.get('/', (req, res) => {
+    res.send('Route not found!  --vinayak');
+  });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
