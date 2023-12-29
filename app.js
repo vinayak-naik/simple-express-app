52617a86cf073cea9c4e1a9f321997c19cc58dba
vinayak-naik <vinayaknaik2021@gmail.com>
2023-12-29 11:16:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// app.js
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+// Define a test route
+app.get('/test', (req, res) => {
+  res.send('This is a test route!');
+});
+app.get('/*', (req, res) => {
+  res.send('Route not found!  --vinayak');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
